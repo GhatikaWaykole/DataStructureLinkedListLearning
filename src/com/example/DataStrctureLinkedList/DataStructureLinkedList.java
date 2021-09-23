b9acc4b31e940a3d423885d432d8ac2967c8ddb6
ghatikawaykole <ghatika99@gmail.com>
2021-09-23 12:53:27 +0530

    Addding node to crate linked list

---
+++ src/com/example/DataStrctureLinkedList/DataStructureLinkedList.java
@@ -0,0 +1,18 @@
+package com.example.DataStrctureLinkedList;
+/* linked list learing problem
+ * create a node class and perform compute on the linked list
+ */
+public class DataStructureLinkedList {
+    //create class node to give data and link
+    static class Node{
+        int data;
+        Node link;
+        Node(int d){
+            data = d;
+            link = null;
+        }
+    }
+    public static void main(String args[]) {
+        System.out.println("welcome to linked list");
+    }
+}
