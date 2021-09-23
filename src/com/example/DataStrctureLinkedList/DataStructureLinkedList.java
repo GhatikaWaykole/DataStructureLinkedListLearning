8d73aa108c9b5eb82bcd7a453edc5501ff850fe5
ghatikawaykole <ghatika99@gmail.com>
2021-09-23 20:58:25 +0530

    Adding node to linked list

--- src/com/example/DataStrctureLinkedList/DataStructureLinkedList.java
+++ src/com/example/DataStrctureLinkedList/DataStructureLinkedList.java
@@ -3,7 +3,21 @@ package com.example.DataStrctureLinkedList;
  * create a node class and perform compute on the linked list
  */
 public class DataStructureLinkedList {
-    public static void main(String args[]){
-        System.out.println("welcome to the linked list");
+    Node head;
+    //create a node for the linked list
+    static class Node{
+        int data;
+        Node next;
+        Node(int d){
+            data = d;
+            next = null;
+        }
     }
-}
+
+    public static void main(String args[]) {
+        System.out.println("welcome to linked list");
+        //create obejct and give to the method
+        DataStructureLinkedList linkedList = new DataStructureLinkedList();
+
+    }
+    }
